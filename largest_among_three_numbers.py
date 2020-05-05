9e8461e716733fbb472b1985fe2a722cbf08254f
ARIF <lone_wolf@ASUS.localdomain>
2020-05-05 19:15:56 +0600

    solve latgest among three numbers problem

---
+++ largest_among_three_numbers.py
@@ -0,0 +1,27 @@
+a=float(input("First number: "))
+b=float(input("Second number: "))
+c=float(input("Third number: "))
+
+if (a>b):
+    if (a>c):
+        print(str(a)+ " is the largest number  ")
+    else:
+        print(str(c)+" is the largest number ")
+else:
+    if (b>c):
+        print(str(b)+" is the largest number ")
+    else:
+        print(str(c)+" is the largest number ")
+
+
+
+
+
+
+
+
+
+
+
+
+
