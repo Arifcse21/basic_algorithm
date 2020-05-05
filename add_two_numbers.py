b866951a78b46d87402070c7355c84199d043e80
ARIF <lone_wolf@ASUS.localdomain>
2020-05-05 19:14:53 +0600

    solve add two numbers problem

---
+++ add_two_numbers.py
@@ -0,0 +1,17 @@
+num1=float(input("Enter the 1st number: "))
+num2=float(input("Enter the 2nd number: "))
+sum=round((num1+num2),2)
+print("Summation of 2 numbers is "+str(sum))
+
+
+
+
+
+
+
+
+
+
+
+
+
