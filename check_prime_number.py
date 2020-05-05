8ae9ffe2b9fb4da904394346b8c785fb4215ccd4
ARIF <lone_wolf@ASUS.localdomain>
2020-05-05 19:16:47 +0600

    solve check prime number problem

---
+++ check_prime_number.py
@@ -0,0 +1,14 @@
+n=int(input("Enter a number: "))
+flag = 1
+for i in range(2,int(n/2),1):
+    if(n%i == 0):
+        flag = 0
+        break
+if(flag == 0):
+    print("Your Entered number is NOT PRIME")
+else:
+    print("Your Entered number is PRIME")
+
+
+
+
